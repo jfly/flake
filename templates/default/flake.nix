9b7afc7ba3fb76c4cac739d557f7a09fcd5d5df6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-06-01 15:20:24 -0700

    Add missing import

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
 
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     systems.url = "github:nix-systems/default";
