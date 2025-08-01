ab1f24b481f2f56931bfb03093ede0b1f4114ddb
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-07-31 23:04:10 -0700

    More follows

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -4,15 +4,18 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     systems.url = "github:nix-systems/default";
 
     treefmt-nix = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
